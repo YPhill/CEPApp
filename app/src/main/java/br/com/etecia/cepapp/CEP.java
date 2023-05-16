42e8ca473afc56a937cdde01b5453e6278be8c35
yphill <phelipequeiroz2004@gmail.com>
2023-05-16 20:16:23 -0300

    attdnv

--- app/src/main/java/br/com/etecia/cepapp/CEP.java
+++ app/src/main/java/br/com/etecia/cepapp/CEP.java
@@ -14,5 +14,74 @@ public class CEP {
 
     private String estado;
 
+    public CEP() {
+    }
+
+    public CEP(String cep, String numero, String complemento, String bairro, String cidade, String estado) {
+        this.cep = cep;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "CEP{" + getCep()
+                + "\nNúmero: "  + getNumero()
+                + "\nComplemento: " + getComplemento()
+                + "\nBairro: " + getBairro()
+                + "\nCidade:" + getCidade()
+                + "\nEstado: " + getEstado();
+    }
 }
 
